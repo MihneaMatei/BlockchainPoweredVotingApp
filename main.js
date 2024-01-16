5642d3252650a741581fa2ec9ea117974c036c6c
mihnea.matei <mihnea.matei@stud.acs.upb.ro>
2024-01-16 17:49:53 +0200

    added use cases of buttons

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let WALLET_CONNECTED = "";
-let contractAddress = "0x75Ca2D0E13d0757E7D114bd7BA4A1485929AD9e9";
+let contractAddress = "0x0645a47B8dad7AA2A9F795cb1B58D0164febE075";
 let contractAbi = [
   {
     "inputs": [
@@ -174,24 +174,37 @@ let contractAbi = [
     element.innerHTML = "Metamask is connected " + WALLET_CONNECTED;
 }
 
-const addVote = async() => {
-    if(WALLET_CONNECTED != 0) {
-        var name = document.getElementById("vote");
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        await provider.send("eth_requestAccounts", []);
-        const signer = provider.getSigner();
-        const contractInstance = new ethers.Contract(contractAddress, contractAbi, signer);
-        var cand = document.getElementById("cand");
-        cand.innerHTML = "Please wait, adding a vote in the smart contract";
+const addVote = async () => {
+  if (WALLET_CONNECTED !== 0) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(contractAddress, contractAbi, signer);
+    const status = await contractInstance.getVotingStatus();
+
+    if (status == 1) {
+      var name = document.getElementById("vote");
+
+      var cand = document.getElementById("cand");
+      cand.innerHTML = "Please wait, adding a vote in the smart contract";
+
+      try {
         const tx = await contractInstance.vote(name.value);
         await tx.wait();
-        cand.innerHTML = "Vote added !!!";
+        cand.innerHTML = "Your vote was successfully counted.";
+      } catch (error) {
+        console.error("Error adding vote:", error);
+        cand.innerHTML = "Error adding vote. Please try again.";
+      }
+    } else {
+      var cand = document.getElementById("cand");
+      cand.innerHTML = "Voting has finished. You cannot vote anymore.";
     }
-    else {
-        var cand = document.getElementById("cand");
-        cand.innerHTML = "Please connect metamask first";
-    }
-}
+  } else {
+    var cand = document.getElementById("cand");
+    cand.innerHTML = "Please connect Metamask first.";
+  }
+};
 
 const voteStatus = async() => {
     if(WALLET_CONNECTED != 0) {
