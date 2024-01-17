7254fc1f3d89b6289907be98f50597f944429ca4
Adrian Clapa <adrian.clapa08@gmail.com>
2024-01-17 09:48:46 +0200

    Add SC interaction
    Signed-off-by: Adrian Clapa <adrian.clapa08@gmail.com>

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ function App() {
   const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
+  const [votingStatus, setVotingStatus] = useState(true);
+  const [remainingTime, setremainingTime] = useState('');
+  const [candidates, setCandidates] = useState([]);
   const [number, setNumber] = useState('');
+  const [CanVote, setCanVote] = useState(true);
 
 
   useEffect( () => {
@@ -27,6 +31,75 @@ function App() {
       }
     }
   });
+
+
+  async function vote() {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const contractInstance = new ethers.Contract (
+        contractAddress, contractAbi, signer
+      );
+
+      const tx = await contractInstance.vote(number);
+      await tx.wait();
+      canVote();
+  }
+
+
+  async function canVote() {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const contractInstance = new ethers.Contract (
+        contractAddress, contractAbi, signer
+      );
+      const voteStatus = await contractInstance.voters(await signer.getAddress());
+      setCanVote(voteStatus);
+
+  }
+
+  async function getCandidates() {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const contractInstance = new ethers.Contract (
+        contractAddress, contractAbi, signer
+      );
+      const candidatesList = await contractInstance.getAllVotesOfCandiates();
+      const formattedCandidates = candidatesList.map((candidate, index) => {
+        return {
+          index: index,
+          name: candidate.name,
+          voteCount: candidate.voteCount.toNumber()
+        }
+      });
+      setCandidates(formattedCandidates);
+  }
+
+
+  async function getCurrentStatus() {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const contractInstance = new ethers.Contract (
+        contractAddress, contractAbi, signer
+      );
+      const status = await contractInstance.getVotingStatus();
+      console.log(status);
+      setVotingStatus(status);
+  }
+
+  async function getRemainingTime() {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const contractInstance = new ethers.Contract (
+        contractAddress, contractAbi, signer
+      );
+      const time = await contractInstance.getRemainingTime();
+      setremainingTime(parseInt(time, 16));
+  }
 
   function handleAccountsChanged(accounts) {
     if (accounts.length > 0 && account !== accounts[0]) {
@@ -64,9 +137,21 @@ function App() {
 
   return (
     <div className="App">
-      {<Login connectWallet = {connectToMetamask}/>}
+      { votingStatus ? (isConnected ? (<Connected 
+                      account = {account}
+                      candidates = {candidates}
+                      remainingTime = {remainingTime}
+                      number= {number}
+                      handleNumberChange = {handleNumberChange}
+                      voteFunction = {vote}
+                      showButton = {CanVote}/>) 
+                      
+                      : 
+                      
+                      (<Login connectWallet = {connectToMetamask}/>)) : (<Finished />)}
       
     </div>
   );
 }
+
 export default App;
