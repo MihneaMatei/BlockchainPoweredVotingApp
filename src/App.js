8fc1df3cf05d1aa3d6e6c8f93cc2d2defbd87389
Adrian Clapa <adrian.clapa08@gmail.com>
2024-01-17 09:47:11 +0200

    Metamask login integreation
    Signed-off-by: Adrian Clapa <adrian.clapa08@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import {ethers} from 'ethers';
+import {contractAbi, contractAddress} from './Constant/constant';
+import Login from './Components/Login';
+import Finished from './Components/Finished';
+import Connected from './Components/Connected';
 import './App.css';
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [number, setNumber] = useState('');
+
+
+  useEffect( () => {
+    getCandidates();
+    getRemainingTime();
+    getCurrentStatus();
+    if (window.ethereum) {
+      window.ethereum.on('accountsChanged', handleAccountsChanged);
+    }
+
+    return() => {
+      if (window.ethereum) {
+        window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
+      }
+    }
+  });
+
+  function handleAccountsChanged(accounts) {
+    if (accounts.length > 0 && account !== accounts[0]) {
+      setAccount(accounts[0]);
+      canVote();
+    } else {
+      setIsConnected(false);
+      setAccount(null);
+    }
+  }
+
+  async function connectToMetamask() {
+    if (window.ethereum) {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        console.log("Metamask Connected : " + address);
+        setIsConnected(true);
+        canVote();
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      console.error("Metamask is not detected in the browser")
+    }
+  }
+
+  async function handleNumberChange(e) {
+    setNumber(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {<Login connectWallet = {connectToMetamask}/>}
+      
     </div>
   );
 }
-
 export default App;
